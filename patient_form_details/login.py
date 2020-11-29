dff0b550bf026c0f5e4f013dd72ef0b041a30564
Yaswanthi <yaswanthi2001@gmail.com>
2020-11-29 17:34:38 +0530

    final

--- patient_form_details/login.py
+++ patient_form_details/login.py
@@ -15,22 +15,22 @@ class Login:
         self.username = request.form.get('emailid')
         self.password = request.form.get('passid')
         self.password_encrypted = hashlib.sha256(self.password.encode()).hexdigest()
-        self.session_for_login_details()
+        # self.session_for_login_details()
 
-    def session_for_login_details(self):
-        session['listOfVariable'] = {}
-        session['listOfVariable'].update(
-            {'username': self.username, 'password': self.password_encrypted})
-
-        session.modified = True
-        list_var = {}
-        list_var.update(session['listOfVariable'])
-        self.inserting_into_database_login_details(list_var)
+    # def session_for_login_details(self):
+    #     session['listOfVariable'] = {}
+    #     session['listOfVariable'].update(
+    #         {'username': self.username, 'password': self.password_encrypted})
+    #
+    #     session.modified = True
+    #     list_var = {}
+    #     list_var.update(session['listOfVariable'])
+        self.inserting_into_database_login_details()
 
     def inserting_into_database_login_details(self, list_var):
         cur = self.con.cursor()
         # hashlib.sha256(str2hash.encode()).hexdigest()
         cur.execute(
-                "INSERT INTO login(username, password)VALUES (?,?)", (list_var['username'], list_var['password']))
+                "INSERT INTO login(username, password)VALUES (?,?)", (self.username,self.password_encrypted ))
         self.con.commit()
         print("record added ")
